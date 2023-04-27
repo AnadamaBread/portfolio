2b5a3a666df0e1bc8a4b9b5000112bd1de3557dc
AnadamaBread <lbaez133@gmail.com>
2023-04-26 20:59:33 -0400

    resume added

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,33 +2,61 @@ import React, { Component, useEffect, useState } from "react";
 import winter23 from "../assets/winter23resume.pdf";
 import { Button, Container, Row } from "react-bootstrap";
 import { AiOutlineDownload } from "react-icons/ai";
-import {Document, Page, pdfjs} from "react-pdf";
+import { Document, Page, pdfjs } from "react-pdf";
 
+const resLink =
+"https://raw.githubusercontent.com/AnadamaBread/portfolio/main/src/assets/winter23resume.pdf";
 
 export default function Resume() {
-    const[width, setWidth] = useState(1200);
 
-    useEffect(() => {
-        setWidth(window.innerWidth);
-    }, []);
+  const [width, setWidth] = useState(1200);
+  const [numPages, setNumPages] = useState(null);
+  const [pageNumber, setPageNumber] = useState(1);
 
-    return(
-        <div>
-            <container fluid className="resume-section">
-                <Row className="resume">
-                    <Document file={resLink} className="d-flex justify-content-center">
-                        <Page pageNumber={1} scale={width> 786 ? 1.7 : 0.6}/>
-                    </Document>
-                </Row>
-                <Row style={{justifyContent:"center", position:"relative"}}>
-                    <Button variant="primary" href="winter23" target="_blank" style={{ maxWidth:"350px"}}>
-                        <AiOutlineDownload/>
-                        &nbsp;Download Resume
-                    </Button>
-                    
-                </Row>
-            </container>
-        </div>
-    )
+  function onDocumentLoadSuccess({ numPages }) {
+    setNumPages(numPages);
+  }
 
+  //
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  return (
+    <div>
+      <Container>
+        <Row
+          style={{
+            justifyContent: "center",
+            position: "relative",
+            paddingTop: "30px",
+            paddingBottom: "30px",
+          }}
+        >
+          <Button
+            variant="primary"
+            href={winter23}
+            target="_blank"
+            className="resume-button"
+          >
+            <AiOutlineDownload />
+          </Button>
+        </Row>
+
+        <div style={{ paddingBottom:"50px" }}>
+          <Document
+            file={resLink}
+            options={{ workerSrc: "pdf.worker.js" }}
+            onLoadSuccess={onDocumentLoadSuccess}
+          >
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6 } renderTextLayer={false} renderAnnotationLayer={false} />
+          </Document>
+          </div>
+          <Row>
+
+          </Row>
+      </Container>
+    </div>
+  );
 }
